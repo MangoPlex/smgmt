1b367ef30f10b5cf9629c04dce19750662482123
Do Tran Thai An <justapie06@gmail.com>
2024-08-10 21:39:52 +0700

    fix command error

--- src/main/java/net/justapie/smgmt/commands/moderation/Ban.java
+++ src/main/java/net/justapie/smgmt/commands/moderation/Ban.java
@@ -5,8 +5,14 @@ import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.tree.LiteralCommandNode;
 import com.velocitypowered.api.command.BrigadierCommand;
 import com.velocitypowered.api.command.CommandSource;
+import com.velocitypowered.api.proxy.Player;
 import com.velocitypowered.api.proxy.ProxyServer;
 import net.justapie.smgmt.commands.VCommand;
+import net.justapie.smgmt.utils.Config;
+import net.justapie.smgmt.utils.ConfigFormatter;
+import net.kyori.adventure.text.Component;
+
+import java.util.Optional;
 
 public class Ban extends VCommand {
     public Ban() {
@@ -20,12 +26,27 @@ public class Ban extends VCommand {
                         BrigadierCommand.requiredArgumentBuilder(
                                 "player",
                                 StringArgumentType.word()
-                        ).build()
+                        ).suggests(
+                                (ctx, builder) -> {
+                                    proxy.getAllPlayers().forEach(player -> builder.suggest(player.getUsername()));
+                                    return builder.buildFuture();
+                                }
+                        ).executes(
+                                ctx -> {
+                                    Optional<Player> player = proxy.getPlayer(ctx.getArgument("player", String.class));
+                                    player.ifPresent(value -> value.disconnect(
+                                            Component.text(
+                                                    new ConfigFormatter(
+                                                            Config.getInstance().getConfigNode().node("messages", "ban").getString()
+                                                    )
+                                                            .putKV("reason", "No Reason Provided")
+                                                            .build()
+                                            )
+                                    ));
+                                    return Command.SINGLE_SUCCESS;
+                                }
+                        )
                 )
-                .executes(ctx -> {
-                    ctx.getSource().sendPlainMessage(ctx.getArgument("player", String.class));
-                    return Command.SINGLE_SUCCESS;
-                })
                 .build();
 
         return new BrigadierCommand(banNode);
---
+++ src/main/java/net/justapie/smgmt/utils/ConfigFormatter.java
@@ -0,0 +1,24 @@
+package net.justapie.smgmt.utils;
+
+import org.apache.commons.text.StringSubstitutor;
+
+import java.util.HashMap;
+
+public class ConfigFormatter {
+    private final String s;
+    private final HashMap<String, String> map;
+
+    public ConfigFormatter(String s) {
+        this.s = s;
+        this.map = new HashMap<>();
+    }
+
+    public ConfigFormatter putKV(String k, String v) {
+        map.put(k, v);
+        return this;
+    }
+
+    public String build() {
+        return StringSubstitutor.replace(this.s, this.map, "{", "}");
+    }
+}
