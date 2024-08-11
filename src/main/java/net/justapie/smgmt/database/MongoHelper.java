736d6696513c2b7cd4ccf4107b5db72c7b4ad0e8
Do Tran Thai An <justapie06@gmail.com>
2024-08-11 11:20:47 +0700

    add mongo and morphia odm

---
+++ src/main/java/net/justapie/smgmt/database/MongoHelper.java
@@ -0,0 +1,29 @@
+package net.justapie.smgmt.database;
+
+import com.mongodb.client.MongoClients;
+import dev.morphia.Datastore;
+import dev.morphia.Morphia;
+import net.justapie.smgmt.config.Config;
+import org.spongepowered.configurate.CommentedConfigurationNode;
+
+public class MongoHelper {
+    private static final MongoHelper instance = new MongoHelper();
+    private static final CommentedConfigurationNode databaseNode = Config.getDatabaseNode();
+    private Datastore ds;
+
+    public static MongoHelper getInstance() {
+        return instance;
+    }
+
+    public void initializeDatabase() {
+        ds = Morphia.createDatastore(
+                MongoClients.create(
+                        databaseNode.node("mongodb", "mongodb-connection-string").getString()
+                )
+        );
+    }
+
+    public Datastore getDs() {
+        return this.ds;
+    }
+}
